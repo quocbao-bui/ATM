7226368091a455f0d9b3c0ea97cd4ad0d3e8c9b5
quocbao-bui <116546659+quocbao-bui@users.noreply.github.com>
2022-10-24 17:12:01 +0700

    Add files via upload

---
+++ account.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class account {
+    static class Account {
+        private final int pinNumber;
+        private double Balance = 0;
+        Scanner input = new Scanner(System.in);
+        DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+        public Account(int pinNumber) {
+            this.pinNumber = pinNumber;
+        }
+        public Account(int pinNumber, int Balance) {
+            this.pinNumber = pinNumber;
+            this.Balance = Balance;
+        }
+        public int getPinNumber() {
+            return pinNumber;
+        }
+        public double getBalance() {
+            return Balance;
+        }
+        public void calcWithdraw(double amount) {
+            Balance = (Balance - amount);
+        }
+        public void calcDeposit(double amount) {
+            Balance = (Balance + amount);
+        }
+        void getWithdrawInput() {
+            boolean end = false;
+            while (!end) {
+                try {
+                    System.out.println("\nCurrent Account Balance: " + moneyFormat.format(Balance));
+                    System.out.print("\nAmount you want to withdraw from Account: ");
+                    double amount = input.nextDouble();
+                    if ((Balance - amount) >= 0 && amount >= 0) {
+                        calcWithdraw(amount);
+                        System.out.println("\nCurrent Account Balance: " + moneyFormat.format(Balance));
+                        end = true;
+                    } else {
+                        System.out.println("\nBalance and Amount Cannot be Negative.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("\nInvalid Choice.");
+                    input.next();
+                }
+            }
+        }
+        void getDepositInput() {
+            boolean end = false;
+            while (!end) {
+                try {
+                    System.out.println("\nCurrent Account Balance: " + moneyFormat.format(Balance));
+                    System.out.print("\nAmount you want to deposit from Account: ");
+                    double amount = input.nextDouble();
+                    if (amount >= 0) {
+                        calcDeposit(amount);
+                        System.out.println("\nCurrent Account Balance: " + moneyFormat.format(Balance));
+                        end = true;
+                    } else {
+                        System.out.println("\nBalance and Amount Cannot Be Negative.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("\nInvalid Choice.");
+                    input.next();
+                }
+            }
+        }
+
+    }
+}
--- inout.java
+++ inout.java
@@ -5,9 +5,40 @@ import java.util.*;
 
 public class inout {
     static class OptionMenu {
+        void mainMenu() {
+            data.put(321, new account.Account(321, 1000));
+            data.put(123, new account.Account(123, 200));
+            boolean end = false;
+            while (!end) {
+                try {
+                    System.out.println("\n Type 1 - Login");
+                    System.out.println(" Type 2 - Create Account");
+                    System.out.println(" Type 3 - Exit");
+                    System.out.print("\nChoice: ");
+                    int choice = menuInput.nextInt();
+                    switch (choice) {
+                        case 1:
+                            getLogin();
+                            break;
+                        case 2:
+                            createAccount();
+                            break;
+                        case 3:
+                            end = true;
+                            break;
+                        default:
+                            System.out.println("\nInvalid Choice.");
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("\nAnother Invalid Choice.");
+                    menuInput.next();
+                }
+            }
+            System.out.println("\nThank You for using the ATM.\n");
+        }
         Scanner menuInput = new Scanner(System.in);
         DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
-        HashMap<Integer, ATM.Account> data = new HashMap<>();
+        HashMap<Integer, account.Account> data = new HashMap<>();
         private void getLogin() {
             boolean end = false;
             int customerNumber;
@@ -22,8 +53,8 @@ public class inout {
                     }
                     System.out.print("\nEnter your PIN number: ");
                     pinNumber = menuInput.nextInt();
-                    for (Map.Entry<Integer, ATM.Account> integerAccountEntry : data.entrySet()) {
-                        ATM.Account acc = (ATM.Account) ((Map.Entry<?, ?>) integerAccountEntry).getValue();
+                    for (Map.Entry<Integer, account.Account> integerAccountEntry : data.entrySet()) {
+                        account.Account acc = (account.Account) ((Map.Entry<?, ?>) integerAccountEntry).getValue();
                         if (data.containsKey(customerNumber) && pinNumber == acc.getPinNumber()) {
                             getChecking(acc);
                             end = true;
@@ -40,7 +71,7 @@ public class inout {
                 }
             }
         }
-        private void getChecking(ATM.Account acc) {
+        private void getChecking(account.Account acc) {
             boolean end = false;
             while (!end) {
                 try {
@@ -84,7 +115,7 @@ public class inout {
                         System.out.println("Please enter another number: ");
                         continue;
                     }
-                    for (Map.Entry<Integer, ATM.Account> ignored : data.entrySet()) {
+                    for (Map.Entry<Integer, account.Account> ignored : data.entrySet()) {
                         if (!data.containsKey(cst_no)) {
                             end = true;
                             break;
@@ -100,41 +131,11 @@ public class inout {
             }
             System.out.println("\nEnter PIN to be registered");
             int pin = menuInput.nextInt();
-            data.put(cst_no, new ATM.Account(pin));
+            data.put(cst_no, new account.Account(pin));
             System.out.println("\nYour new account has been successfully registered!");
             System.out.println("\nRedirecting to login.............");
             getLogin();
         }
-        void mainMenu() {
-            data.put(321, new ATM.Account(321, 1000));
-            data.put(123, new ATM.Account(123, 200));
-            boolean end = false;
-            while (!end) {
-                try {
-                    System.out.println("\n Type 1 - Login");
-                    System.out.println(" Type 2 - Create Account");
-                    System.out.println(" Type 3 - Exit");
-                    System.out.print("\nChoice: ");
-                    int choice = menuInput.nextInt();
-                    switch (choice) {
-                        case 1:
-                            getLogin();
-                            break;
-                        case 2:
-                            createAccount();
-                            break;
-                        case 3:
-                            end = true;
-                            break;
-                        default:
-                            System.out.println("\nInvalid Choice.");
-                    }
-                } catch (InputMismatchException e) {
-                    System.out.println("\nAnother Invalid Choice.");
-                    menuInput.next();
-                }
-            }
-            System.out.println("\nThank You for using the ATM.\n");
-        }
+
     }
 }
